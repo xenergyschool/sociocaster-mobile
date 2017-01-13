0c07810eaff889d2ba3b6cbeed510cb9fef1f1fd
Asep Koswara <asep.koswara@gmail.com>
2017-01-13 16:54:22 +0700

    add unavailable image

--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,5 @@
 export { default as logoBlue } from './sc-logo-blue.png'
 
 export { default as defaultAvatar } from './default-avatar.png'
+
+export { default as unavailableImage } from './unavailable.png'
